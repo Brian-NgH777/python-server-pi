1b1b77da4a5aaf490fb010b68012ec965e77060a
Brian nguyen <brian.ng142957@gmail.com>
2021-12-17 13:29:22 +0700

    Edit

--- helper/scapy.py
+++ helper/scapy.py
@@ -1,5 +1,4 @@
-import scapy.all
-from scapy.all import *
+from scapy.all import ARP,Ether,srp
 import urllib.request as req
 import argparse
 
@@ -17,12 +16,12 @@ import argparse
 #     return options
   
 def scan(ip):
-    arp_req_frame = scapy.ARP(pdst = ip)
-    broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    arp_req_frame = ARP(pdst = ip)
+    broadcast_ether_frame = Ether(dst = "ff:ff:ff:ff:ff:ff")
     
     broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
 
-    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
+    answered_list = srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
     result = []
     for i in range(0,len(answered_list)):
         client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc, "vendor": resolveMac(answered_list[i][1].hwsrc)}

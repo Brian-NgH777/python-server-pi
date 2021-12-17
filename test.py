29374b6c50ffe581834475d0805f33bdea28af80
Brian nguyen <brian.ng142957@gmail.com>
2021-12-17 14:48:39 +0700

    Edit

--- test.py
+++ test.py
@@ -3,10 +3,8 @@ import helper.scapy as scapyHelper
 import subprocess
 
 if __name__ == "__main__":
-    network = subprocess.call(["hostname","-I"])
-    print("networknetworknetwork", network)
     cidr= "/24" # /24	255.255.255.0	254
-    ip= "%s%s" %(network, cidr)
+    ip= "%s%s" %(subprocess.call(["hostname","-I"]), cidr)
     print("ippppppppp", ip)
     data = scapyHelper.scan(ip)
     scapyHelper.display_result(data)

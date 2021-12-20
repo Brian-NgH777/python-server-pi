78495cbaeeb51a664209bf6080a67d7a4c28e062
Brian nguyen <brian.ng142957@gmail.com>
2021-12-20 22:54:50 +0700

    Edit

--- helper/streaming.py
+++ helper/streaming.py
@@ -1,3 +1,4 @@
+import sys
 import threading
 import subprocess
 
@@ -6,11 +7,17 @@ cmd2 = "ffmpeg -fflags nobuffer -rtsp_transport tcp -i %s -framerate 20 -video_s
 # cmd3 = "ffmpeg -fflags nobuffer -rtsp_transport tcp -i rtsp://admin:Viact123@192.168.92.111/live -framerate 20 -video_size 720x480 -vcodec libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -vf \"format=yuv420p\" -g 60 -c:a aac -b:a 128k -ar 44100 -f flv rtmp://54.254.0.41/live/test3"
 
 def thread_function(rtsp, rtmp):
-    try:
-        r = cmd2%(rtsp, rtmp)
-        subprocess.Popen(r, shell=True)
-    except subprocess.CalledProcessError as e:
-         print(e.output)
+    # try:
+        # r = cmd2%(rtsp, rtmp)
+        # subprocess.Popen(r, shell=True)
+
+    process = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    my_pid, err = process.communicate()
+    if len(my_pid.splitlines()) >0:
+        print("Script Running in background")
+        sys.exit(0);
+    # except subprocess.CalledProcessError as e:
+    #      print(e.output)
    
 def new(rtsp, rtmp):
     thread_function(rtsp, rtmp)

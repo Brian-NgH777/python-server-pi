9bc92a3048ce420c2aca1c60fa445affe67f1b4d
Brian nguyen <brian.ng142957@gmail.com>
2021-12-21 09:23:21 +0700

    Edit

--- test2.py
+++ test2.py
@@ -20,7 +20,7 @@ def thread_function(thead):
     elif thead == 1 :
         # Popen(cmd2, shell=True, stdout=DEVNULL)
         #  os.spawnl(os.P_DETACH, cmd2)
-        subprocess.call("ffmpeg -fflags nobuffer -rtsp_transport tcp -i rtsp://admin:Viact123@192.168.92.111/live -framerate 20 -video_size 400x400 -vcodec libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -vf \"format=yuv420p\" -g 60 -c:a aac -b:a 128k -ar 44100 -f flv rtmp://54.254.0.41/live/test2 &> /dev/null", shell=True, stdout=DEVNULL)
+        subprocess.call("ffmpeg -fflags nobuffer -rtsp_transport tcp -i rtsp://admin:Viact123@192.168.92.111/live -framerate 20 -video_size 400x400 -vcodec libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -vf \"format=yuv420p\" -g 60 -c:a aac -b:a 128k -ar 44100 -f flv rtmp://54.254.0.41/live/test2", shell=True, stdout=DEVNULL)
         # os.system(cmd2)
     # elif thead == 2 :
     #     subprocess.check_output(cmd3, shell=True).decode("utf-8")

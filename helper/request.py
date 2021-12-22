6e796c69022c847604206be73aeb44b37e299bdc
Brian nguyen <brian.ng142957@gmail.com>
2021-12-22 20:19:44 +0700

    Edit

--- helper/request.py
+++ helper/request.py
@@ -1,17 +1,17 @@
 import requests
-import time
+# import time
 from requests.exceptions import HTTPError
 from requests.auth import HTTPBasicAuth
 from requests.auth import HTTPDigestAuth
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+# from requests.adapters import HTTPAdapter
+# from requests.packages.urllib3.util.retry import Retry
 
-requests.adapters.DEFAULT_RETRIES = 5
-session = requests.Session()
-session.keep_alive = False
-retry = Retry(connect=3, backoff_factor=0.5)
-adapter = HTTPAdapter(max_retries=retry)
-session.mount('http://', adapter)
+# requests.adapters.DEFAULT_RETRIES = 5
+# session = requests.Session()
+# session.keep_alive = False
+# retry = Retry(connect=3, backoff_factor=0.5)
+# adapter = HTTPAdapter(max_retries=retry)
+# session.mount('http://', adapter)
 
 timeout = 5
 
@@ -51,7 +51,7 @@ def PostFile(url="", path=""):
     try:
         files = {'file': open(path, 'rb')}
         # headers = {'Content-type': 'multipart/form-data'}
-        r = session.post(url, files=files)
+        r = requests.post(url, files=files)
         r.raise_for_status()
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')

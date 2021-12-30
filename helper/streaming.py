59e76fc22cb4f7dffd202110709754f926a1eab6
Brian nguyen <brian.ng142957@gmail.com>
2021-12-30 09:16:06 +0700

    Edit cmd

--- helper/streaming.py
+++ helper/streaming.py
@@ -4,7 +4,7 @@ import threading
 import subprocess
 
 cmdStart = "nohup ffmpeg -fflags nobuffer -rtsp_transport tcp -i %s -framerate 20 -video_size 480x320 -vcodec libx264 -preset veryfast -maxrate 1984k -bufsize 3968k -vf \"format=yuv420p\" -g 60 -c:a aac -b:a 128k -ar 44100 -f flv %s &"
-cmdStop = "sudo sh stop_stream.sh"
+cmdStop = "sudo ./stop_stream.sh"
 
 def thread_function(rtsp, rtmp):
     try:
